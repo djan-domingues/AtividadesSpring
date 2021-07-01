9d1290ec922f4e84c119c3fa4cf3f1aa2e9aecdf
Djan Domingues <felipej.domingues@gmail.com>
2021-07-01 09:49:16 -0300

    Atividade Spring 01-07

---
+++ farmaciaGen/farmaciaGen/src/main/java/com/farmaciaGen/farmaciaGen/model/Produto.java
@@ -0,0 +1,75 @@
+package com.farmaciaGen.farmaciaGen.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "Produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private int qntd_estoque;
+	
+	@NotNull
+	private float preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQntd_estoque() {
+		return qntd_estoque;
+	}
+
+	public void setQntd_estoque(int qntd_estoque) {
+		this.qntd_estoque = qntd_estoque;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	
+}
---
+++ farmaciaGen/farmaciaGen/src/test/java/com/farmaciaGen/farmaciaGen/FarmaciaGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmaciaGen.farmaciaGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
