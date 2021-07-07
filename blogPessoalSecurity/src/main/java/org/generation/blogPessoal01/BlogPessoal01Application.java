9d56b092fa1ea5b7fc5b7f31bfd3e7375fef7b8c
Djan Domingues <felipej.domingues@gmail.com>
2021-07-06 23:38:22 -0300

    Atualizacao de security no blog pessoal

---
+++ blogPessoalSecurity/src/main/java/org/generation/blogPessoal01/BlogPessoal01Application.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoal01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoal01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoal01Application.class, args);
+	}
+
+}
