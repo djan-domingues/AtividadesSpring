7bd36e30ba2376078c3777941c1df11d564540ba
Djan Domingues <felipej.domingues@gmail.com>
2021-07-12 16:21:57 -0300

    Integrando security

---
+++ minhaLojaDeGamesSecurity/src/main/java/com/minhaLojaDeGames/minhaLojaDeGames/model/Produto.java
@@ -0,0 +1,86 @@
+package com.minhaLojaDeGames.minhaLojaDeGames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "Produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private int qntd_estoque;
+	
+	@NotNull
+	private int preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Usuario usuario;
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getQntd_estoque() {
+		return qntd_estoque;
+	}
+
+	public void setQntd_estoque(int qntd_estoque) {
+		this.qntd_estoque = qntd_estoque;
+	}
+
+	public int getPreco() {
+		return preco;
+	}
+
+	public void setPreco(int preco) {
+		this.preco = preco;
+	}
+
+}
---
+++ minhaLojaDeGamesSecurity/src/main/java/com/minhaLojaDeGames/minhaLojaDeGames/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.minhaLojaDeGames.minhaLojaDeGames.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.minhaLojaDeGames.minhaLojaDeGames.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+	
+	public Optional<Usuario> findByUsuario(String usuario);
+	
+}
